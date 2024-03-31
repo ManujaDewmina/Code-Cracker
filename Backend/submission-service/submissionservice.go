a621d38280a9cc2ae8ea689e16573fec8e426302
Manuja Dewmina <manuja1bc@gmail.com>
2024-03-31 22:26:08 +0530

    Bug Fix submission service

--- Backend/submission-service/submissionservice.go
+++ Backend/submission-service/submissionservice.go
@@ -282,11 +282,11 @@ func uploadSubmission(c *gin.Context) {
 		return
 	}
 
-	// err = os.RemoveAll("temp")
-	// if err != nil {
-	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete temp folder: " + err.Error()})
-	// 	return
-	// }
+	err = os.RemoveAll("temp")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete temp folder: " + err.Error()})
+		return
+	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "Submission created successfully"})
 }
